d790802570347ededcd52a40c0e89db1a25ee8da
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-11-27 15:00:08 +0100

    nil

--- workflow.rb
+++ workflow.rb
@@ -17,6 +17,7 @@ module TSVWorkflow
   input :format, :select, "Format to change to",nil, :select_options => ["Ensembl Gene ID", "Associated Gene Name", "UniProt/SwissProt Accession"]
   input :organism, :string, "Organism code", 'Hsa'
   input :tsv, :tsv, "TSV file to process", nil
+  desc "Change the format of the first column (key) of a TSV file"
   task :change_key => :tsv do |format,organism,tsv|
     tsv.identifiers = Organism.identifiers(organism)
     tsv.change_key format, :persist => true
@@ -27,6 +28,7 @@ module TSVWorkflow
   input :format, :select, "Format to change to",nil, :select_options => ["Ensembl Gene ID", "Associated Gene Name", "UniProt/SwissProt Accession"]
   input :organism, :string, "Organism code", 'Hsa'
   input :tsv, :tsv, "TSV file to process", nil
+  desc "Change the format of a field (not the `key` or first column) of a TSV file"
   task :swap_id => :tsv do |field,format,organism,tsv|
     tsv.identifiers = Organism.identifiers(organism)
     tsv.swap_id field, format, :persist => true
@@ -36,6 +38,7 @@ module TSVWorkflow
   input :format, :select, "Field format to add",nil, :select_options => ["Ensembl Gene ID", "Associated Gene Name", "UniProt/SwissProt Accession"]
   input :organism, :string, "Organism code", 'Hsa'
   input :tsv, :tsv, "TSV file to process", nil
+  desc "Change the format of any column of a TSV file and add it as an additional field"
   task :add_id => :tsv do |format,organism,tsv|
     identifiers = Organism.identifiers(organism).tsv :persist => true
     orig_type = tsv.type 
@@ -48,6 +51,7 @@ module TSVWorkflow
   input :tsv, :tsv, "Original file", nil
   input :new, :tsv, "File to attach", nil
   input :organism, :string, "Organism code", 'Hsa'
+  desc "Attach the fields of `new` to `tsv` by matching a common field"
   task :attach => :tsv do |tsv, new ,organism|
     tsv.identifiers ||=  Organism.identifiers(organism)
     tsv.attach new
